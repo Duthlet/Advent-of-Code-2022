3240b071d9d699827248e6d456fcb18da3bbb8b3
Duthlet <peacefulwesnoth@gmail.com>
2022-12-07 09:17:28 +0100

    Day 7: shorter version without a tree

--- day_7/main.cpp
+++ day_7/main.cpp
@@ -23,36 +23,8 @@ size_t split(const std::string &txt, std::vector<std::string> &strs, char ch)
     return strs.size();
 }
 
-struct node {
-    std::vector<node> children;
-    node* parent;
-    size_t size;
-    std::string name;
-
-    node (std::string nm, node* p) : name(nm), parent(p), size(0), children() {};
-    size_t add_size (std::vector<size_t>& vec);
-    std::string path ();
-};
-
-std::string node::path () {
-    return (parent ? parent->path() : std::string{""}).append("/").append(name);
-}
-
-size_t node::add_size(std::vector<size_t>& vec) {
-    for (auto &c : children) {
-        size += c.add_size(vec);
-    }
-    vec.push_back(size);
-   // std::cout << /*path() << ": " <<*/ size << "\n";
-    return size;
-}
-
-int main () {
-
-    node root{"/", nullptr};
-
-    node *current = &root;
-
+size_t add_size(std::vector<size_t>& vec) {
+    size_t size = 0;
     while (1) {
         std::string line;
         std::getline(std::cin, line);
@@ -60,50 +32,44 @@ int main () {
 
         std::vector<std::string> pl;
         split (line, pl, ' ');
-        if (pl[0] == "$") {
-            if (pl[1] == "cd") {
-                if (pl[2] == "..") {
-                    current = current->parent;
-                } else if (pl[2] == "/") {
-                    current = &root;
-                } else {
-                    current->children.push_back(node{pl[2], current});
-                    current = &(current->children.back());
-                }
-            } else {
-            }
-        } else if (pl[0] == "dir") {
-        } else {
-            current->size += stoi(pl[0]);
-        }
-    }
-    
-    
-    std::cout << "here" << std::endl;
 
+        if (pl[0] == "dir") continue;
+        if (pl[0] == "$") {
+            if (pl[1] != "cd") continue;
+            if (pl[2] == "..") break;
+            size += add_size(vec);
+            if (std::cin.eof()) break;
+            continue;
+        }
+        size += stoi(pl[0]);
+    }
+    vec.push_back(size);
+    return size;
+}
+
+int main () {
+    // get rid of cd /
+    std::string dummy;
+    std::getline(std::cin, dummy);
 
     std::vector<size_t> sizes;
-    root.add_size (sizes);
+    size_t total = add_size (sizes);
 
-    
-    long long total = 0;
+    size_t part1 = 0;
 
     for (auto &s : sizes) {
-        if (s <= 100000) total += s;
+        if (s <= 100000) part1 += s;
     }
 
-    std::cout << total << "\n";
-
-    total = root.size;
-
-    std::cout << total << "\n";
+    std::cout << part1 << "\n";
 
     long to_delete = total - 40000000;
-    std::cout << "to delete: " << to_delete << "\n";
 
     std::sort(sizes.begin(), sizes.end());
     for (auto &s : sizes) {
-        std::cout << s << "\n";
-        if (s >= to_delete) break;
+        if (s >= to_delete) {
+            std::cout << s << "\n";
+            break;
+        }
     }
 }
