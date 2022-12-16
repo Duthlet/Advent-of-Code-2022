b2da77482745be5d82b906bbbf73e0a6589b78fd
Duthlet <peacefulwesnoth@gmail.com>
2022-12-16 11:48:46 +0100

    Solve part 1 of day 16

--- day_16/main.cpp
+++ day_16/main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <map>
+#include <algorithm>
 
 struct valve {
     valve(std::string _name, int _fr, std::vector<std::string>_tun) : name(_name), flowrate(_fr), tunnels() {
@@ -16,6 +17,7 @@ struct valve {
 };
 
 #include "input.inc"
+//#include "test_input.inc"
 
 void print_valve (valve &v) {
     std::cout << "Valve " << v.name << ": flowrate = " << v.flowrate
@@ -64,56 +66,64 @@ int dsf(
     int remaining_moves,
     int flowrate,
     int released_pressure,
-    std::vector<valve> &valves,
-    const std::string &current_valve)
+    const std::string &current_valve,
+    std::vector<std::string> open_valves)
 {
-    int max_pressure = released_pressure + flowrate * remaining_moves;
-    auto cur_it = find_valve(valves, current_valve);
-    if (cur_it == valves.end()) {
+    //std::cout << remaining_moves << "\n";
+    auto cur_it = find_valve(input, current_valve);
+    if (cur_it == input.end()) {
         std::cout << "valve " << current_valve << " not found!\n";
         return 0;
     }
-    // don't turn on valve
+    open_valves.push_back(current_valve);
+    int max_pressure = released_pressure + flowrate + (flowrate + cur_it->flowrate) * remaining_moves;
     for (auto it : cur_it->tunnels) {
-        if (it.second < remaining_moves) {
-            int candidate = dsf(
-                remaining_moves - it.second,
-                flowrate,
-                released_pressure + flowrate * it.second,
-                valves,
-                it.first);
-
-            if (candidate > max_pressure) max_pressure = candidate;
-        }
-    }
-    // turn on valve
-    {
-        int candidate = released_pressure + flowrate
-                        + (flowrate + cur_it->flowrate) * (remaining_moves - 1);
-        if (candidate > max_pressure) max_pressure = candidate;
-    }
-    for (auto it : cur_it->tunnels) {
+        if (std::find(open_valves.begin(), open_valves.end(), it.first) != open_valves.end()) continue;
         if (it.second < remaining_moves-1) {
-            auto new_valves = valves;
-            auto new_it = find_valve(new_valves, current_valve);
-            if (new_it == new_valves.end()) {
-                std::cout << "valve " << current_valve << " not found (in new vector)!\n";
-                return 0;
-            }
-            replace_valve(new_valves, new_it);
-
             int candidate = dsf(
                 remaining_moves - it.second - 1,
                 flowrate + cur_it->flowrate,
                 released_pressure + flowrate + (flowrate + cur_it->flowrate) * it.second,
-                new_valves,
-                it.first);
+                it.first,
+                open_valves);
 
             if (candidate > max_pressure) max_pressure = candidate;
         }
     }
-
     return max_pressure;
+}
+
+std::map<std::string, int>::iterator get_lowest(std::map<std::string, int> &t, std::vector<std::string> &except) {
+    auto lowest_it = t.end();
+    for (auto t_it = t.begin(); t_it != t.end(); ++t_it) {
+        if (std::find(except.begin(), except.end(), t_it->first) != except.end()) continue;
+        if (lowest_it == t.end()) lowest_it = t_it;
+        if (lowest_it->second > t_it->second) lowest_it = t_it;
+    }
+    return lowest_it;
+}
+
+void update (std::vector<valve> &valve_vector, std::vector<valve>::iterator update_valve_it) {
+    std::vector<std::string> updated;
+    auto &t = update_valve_it->tunnels;
+    // add all other valves to tunnel list
+    for (auto v : valve_vector) {
+        if (v.name == update_valve_it->name) continue;
+        auto t_it = t.find(v.name);
+        if(t_it == t.end()) {
+            t[v.name] = 1000;
+        }
+    }
+    // update eacj tunnel
+    while (updated.size() < t.size()) {
+        auto lowest_it = get_lowest(t, updated);
+        updated.push_back(lowest_it->first);
+        for (auto &tun : find_valve(valve_vector, lowest_it->first)->tunnels) {
+            if (tun.first == update_valve_it->name) continue;
+            if (t[tun.first] > lowest_it->second + tun.second)
+                t[tun.first] = lowest_it->second + tun.second;
+        }
+    }
 }
 
 int main () {
@@ -125,10 +135,13 @@ int main () {
             ++it;
         }
     }
+    for (auto it = input.begin(); it != input.end(); ++it) {
+        update(input, it);
+    }
 
     for (auto &v : input) {
         print_valve(v);
     }
 
-    std::cout << dsf(30, 0, 0, input, "AA") << "\n";
+    std::cout << dsf(30, 0, 0, "AA", std::vector<std::string>{}) << "\n";
 }
