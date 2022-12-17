6b53c526e94b6ba6f3ac6e0e2f8012e496803ffa
Duthlet <peacefulwesnoth@gmail.com>
2022-12-17 09:14:06 +0100

    Solve day 17 (hopefully for arbitary input)

--- day_17/main.cpp
+++ day_17/main.cpp
@@ -16,56 +16,60 @@ bool operator== (const point a, const point b) {
     return (a.x == b.x) && (a.y == b.y);
 }
 
+struct cache_t {
+    int index;
+    std::set<point> ground;
+    long long piece;
+    long long height;
+};
+
 //globals
 std::string input {};
 std::set<point> ground {{0,0},{1,0},{2,0},{3,0},{4,0},{5,0},{6,0}};
 long long erased_height = 0;
 int input_index = 0;
-constexpr long long n = 1000000000000;
-//constexpr long long n = 10000;
-std::vector<std::pair<int, std::set<point>>> cache {};
+constexpr long long n_pt1 = 2022;
+constexpr long long n_pt2 = 1000000000000;
+constexpr long long impassable_n = 3;
+std::vector<cache_t> cache {};
+long long  piece_number = 0;
 
-
-
-struct piece {
-    std::vector<point> hitbox;
-};
-piece create_falling_piece (int height) {
+std::vector<point> create_falling_piece (int height) {
     static int i = 0;
 
-    piece retval {};
+    std::vector<point> retval {};
     switch (i%5) {
         case 0:
-            retval.hitbox.push_back(point{2,height+4});
-            retval.hitbox.push_back(point{3,height+4});
-            retval.hitbox.push_back(point{4,height+4});
-            retval.hitbox.push_back(point{5,height+4});
+            retval.push_back(point{2,height+4});
+            retval.push_back(point{3,height+4});
+            retval.push_back(point{4,height+4});
+            retval.push_back(point{5,height+4});
             break;
         case 1:
-            retval.hitbox.push_back(point{2,height+5});
-            retval.hitbox.push_back(point{3,height+4});
-            retval.hitbox.push_back(point{3,height+5});
-            retval.hitbox.push_back(point{3,height+6});
-            retval.hitbox.push_back(point{4,height+5});
+            retval.push_back(point{2,height+5});
+            retval.push_back(point{3,height+4});
+            retval.push_back(point{3,height+5});
+            retval.push_back(point{3,height+6});
+            retval.push_back(point{4,height+5});
             break;
         case 2:
-            retval.hitbox.push_back(point{2,height+4});
-            retval.hitbox.push_back(point{3,height+4});
-            retval.hitbox.push_back(point{4,height+4});
-            retval.hitbox.push_back(point{4,height+5});
-            retval.hitbox.push_back(point{4,height+6});
+            retval.push_back(point{2,height+4});
+            retval.push_back(point{3,height+4});
+            retval.push_back(point{4,height+4});
+            retval.push_back(point{4,height+5});
+            retval.push_back(point{4,height+6});
             break;
         case 3:
-            retval.hitbox.push_back(point{2,height+4});
-            retval.hitbox.push_back(point{2,height+5});
-            retval.hitbox.push_back(point{2,height+6});
-            retval.hitbox.push_back(point{2,height+7});
+            retval.push_back(point{2,height+4});
+            retval.push_back(point{2,height+5});
+            retval.push_back(point{2,height+6});
+            retval.push_back(point{2,height+7});
             break;
         case 4:
-            retval.hitbox.push_back(point{2,height+4});
-            retval.hitbox.push_back(point{2,height+5});
-            retval.hitbox.push_back(point{3,height+4});
-            retval.hitbox.push_back(point{3,height+5});
+            retval.push_back(point{2,height+4});
+            retval.push_back(point{2,height+5});
+            retval.push_back(point{3,height+4});
+            retval.push_back(point{3,height+5});
             break;
     };
     ++i;
@@ -76,23 +80,15 @@ int get_height() {
     return ground.rbegin()->y;
 }
 
-
-void simulate_fall (piece &rock) {
-
-    piece shifted_rock;
+void simulate_fall (std::vector<point> &rock) {
+    std::vector<point> shifted_rock;
     bool shift;
     while (1) {
         //shift rock
         shifted_rock = rock;
         shift = true;
-        /*
-        if (input_index >= input.length()) {
-            std::cout << "end of input\n";
-            input_index = 0;
-        }
-        */
         int i = input_index % input.length();
-        for (auto &p : shifted_rock.hitbox) {
+        for (auto &p : shifted_rock) {
             if (input[i] == '<') {
                 if (p.x == 0) shift = false;
                 --p.x;
@@ -109,7 +105,7 @@ void simulate_fall (piece &rock) {
         //drop rock
         shifted_rock = rock;
         shift = true;
-        for (auto &p : shifted_rock.hitbox) {
+        for (auto &p : shifted_rock) {
             --p.y;
             if (ground.find(p) != ground.end()) shift = false;
         }
@@ -123,76 +119,76 @@ void simulate_fall (piece &rock) {
 
 bool complete (int y) {
     for (int x = 0; x < 7; ++x) {
-        if (ground.find(point{x,y}) == ground.end()) return false;
+        bool b = true;
+        for (int i = 0; i < impassable_n; ++i)
+            b &= (ground.find(point{x,y-i}) == ground.end());
+        if (b) return false;
     }
     return true;
 }
 
 void erase (int y) {
+    static bool caching = true;
+
     std::set<point> new_ground {};
-    for (auto it = ground.find(point{0,y}); it != ground.end(); ++it) {
+    for (auto it = ground.lower_bound(point{0,y}); it != ground.end(); ++it) {
         new_ground.insert(point{it->x, it->y - y});
     }
     ground = new_ground;
-    erased_height += (long long) y;
-/*
-    for (auto &pair : cache) {
-        if ((((input_index - pair.first) % input.length()) == 0) && (pair.second == new_ground)) {
-            std::cout << "cache hit on index " << input_index << "with previous" << pair.first << "\n";
+    erased_height += y;
+
+    if (!caching) return;
+
+    cache_t new_entry {input_index, new_ground, piece_number, erased_height};
+    for (auto &entry : cache) {
+        if ((((new_entry.index - entry.index) % input.length()) == 0)
+            && (new_entry.ground == entry.ground)
+            && ((new_entry.piece - entry.piece) % 5 == 0))
+        {
+            if (new_entry.piece <= n_pt1) break;
+
+            std::cout << "cache hit!\n";
+            std::cout << "old: piece number = " << entry.piece
+                      << ", base height = " << entry.height << "\n";
+            std::cout << "new: piece number = " << new_entry.piece
+                      << ", base height = " << new_entry.height << "\n";
+
+            int piece_diff = new_entry.piece - entry.piece;
+            int height_diff = new_entry.height - entry.height;
+            long long times = (n_pt2 - new_entry.piece) / piece_diff;
+            piece_number += piece_diff * times;
+            erased_height += height_diff * times;
+            caching = false;
+
+            std::cout << "forwarding to: piece number = " << piece_number
+                      << ", base height = " << erased_height << "\n";
+            break;
         }
     }
-    cache.emplace_back(input_index, new_ground);
-*/
-// input_index = 37922
-// input_index = 7649
-// input_diff : 0
+    cache.push_back(new_entry);
 }
 
-void update_ground (piece &rock) {
-    for (auto &p : rock.hitbox) {
+void update_ground (std::vector<point> &rock) {
+    for (auto &p : rock) {
         if (!ground.insert(p).second)
             std::cout << "update failed\n";
     }
-    int y = rock.hitbox[0].y;
-    if ((y >= 1000) && complete(y)) {
-        //std::cout << "found complete row " << erased_height + (long long)y << "\n";
-        erase(y);
+    int y = rock[0].y;
+    if (complete(y)) {
+        erase(y-impassable_n+1);
     }
 }
-//7649: 1300 erased: 2029
-//cache hit on index 37922with previous7649
-//37922: 6400 erased: 9955
-
-// i_diff = 5100.
-// erased_diff = 7926
 
 int main () {
     getline (std::cin, input);
-    std::cout << input.length() << "\n";
 
-    for (long long i = 0; i <= n-1; ++i) {
+    for (; piece_number <= n_pt2-1; ++piece_number) {
+        if (piece_number == n_pt1) {
+            std::cout << "Part 1: " << erased_height + get_height() << "\n";
+        }
         auto rock = create_falling_piece(get_height());
         simulate_fall(rock);
         update_ground(rock);
-
-        if (i == 1300) {
-            while (i < n-1) {
-                erased_height += 7926;
-                i += 5100;
-            }
-            erased_height -= 7926;
-            i -= 5100;
-        }
-
-/*
-        if ((input_index == 37922) || (input_index == 7649))
-            std::cout << input_index << ": " << i << " erased: " << erased_height << "\n";
-*/
-//        for (auto &p : ground) {
-            //std::cout << "(" << p.x << "," << p.y << "), ";
-//        }
-//        std::cout << "\n";
     }
-
-    std::cout << erased_height + (long long)get_height() << "\n";
+    std::cout << "Part 2: " << erased_height + get_height() << "\n";
 }
