820934de7a2bdf0eac00640dec28a3793c8a8df4
Duthlet <peacefulwesnoth@gmail.com>
2022-12-20 14:58:03 +0100

    Solve day 20

--- day_20/main.cpp
+++ day_20/main.cpp
@@ -3,16 +3,9 @@
 #include <list>
 
 #include "input.inc"
-/*std::vector<long long> input
-{1,
-2,
--3,
-3,
--2,
-0,
-4};*/
+
 int size;
-std::list<int> shuffled {};
+std::list<int> shuffled;
 
 void shuffle() {
     for (int n = 0; n < size; ++n) {
@@ -20,15 +13,9 @@ void shuffle() {
         for (; it != shuffled.end(); ++it) if (*it == n) break;
         if (it == shuffled.end()) std::cout << "oops\n";
 
-//        for (auto &x : shuffled) std::cout << input[x] << ", ";
-//        std::cout << "\n";
-
         it = shuffled.erase(it);
         long long cnt = input[n] % (size-1);
         if (cnt < 0) cnt += (size-1);
-
-//        std::cout << "moving " << input[n] << " by " << cnt << "(mod " << size-1<< ")\n";
-
         while (cnt > 0) {
             if (it == shuffled.end()) {
                 it = shuffled.begin();
@@ -40,31 +27,46 @@ void shuffle() {
     }
 }
 
-int main () {
-    size = input.size();
-    for (int i = 0; i < size; ++i) {
-        shuffled.push_back(i);
-    }
-
-    long long key = 811589153;
-    for (auto &i : input) i *= key;
-
-//        for (auto &x : shuffled) std::cout << input[x] << ", ";
-//        std::cout << "\n";
-    for (int i = 0; i < 10; ++i) {
-    shuffle();
-}
+long long get_sum () {
     long long sum = 0;
-
     auto it = shuffled.begin();
     for (; it != shuffled.end(); ++it) if (input[*it] == 0) break;
 
-for (int cnt = 0; cnt < 3; ++cnt) {
-    for (int i = 0; i < 1000; ++i) {
-        ++it;
-        if (it == shuffled.end()) it = shuffled.begin();
+    for (int cnt = 0; cnt < 3; ++cnt) {
+        for (int i = 0; i < 1000; ++i) {
+            ++it;
+            if (it == shuffled.end()) it = shuffled.begin();
+        }
+        sum += input[*it];
     }
-    sum += input[*it];
+    return sum;
 }
-    std::cout << sum << "\n";
+
+void part1 () {
+    shuffled = std::list<int> {};
+    for (int i = 0; i < size; ++i) {
+        shuffled.push_back(i);
+    }
+    shuffle();
+    std::cout << "Part 1: " << get_sum() << "\n";
+}
+
+void part2 () {
+    long long key = 811589153;
+    for (auto &i : input) i *= key;
+
+    shuffled = std::list<int> {};
+    for (int i = 0; i < size; ++i) {
+        shuffled.push_back(i);
+    }
+    for (int i = 0; i < 10; ++i) {
+        shuffle();
+    }
+    std::cout << "Part 2: " << get_sum() << "\n";
+}
+
+int main () {
+    size = input.size();
+    part1();
+    part2();
 }
